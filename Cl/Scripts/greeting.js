15036eb9af41836b0db562f789e2a7d3758e49f4
Francisco Jacques <s3526812@students.fscj.edu>
2024-11-24 13:19:10 -0500

    Add files via upload

---
+++ Cl/Scripts/greeting.js
@@ -0,0 +1,17 @@
+/*
+Author : Francisco Jacques
+Date : 11\24\2024
+Description: This script animates the footer emojis
+*/
+const greetings = [
+    { text: "Hello!", class: "hello" },
+    { text: "Bonjour!", class: "bonjour" },
+    { text: "¡Hola!", class: "hola" }
+];
+let index = 0;
+
+setInterval(() => {
+    const greetingContainer = document.getElementById('greeting-container');
+    greetingContainer.innerHTML = `<span class="greeting ${greetings[index].class}">${greetings[index].text}</span>`;
+    index = (index + 1) % greetings.length;
+}, 2000);
